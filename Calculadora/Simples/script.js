32efc5d5c7f3a7afb59cc6382fc66c975f0c55b7
Mr-R4F <rafael.soares0777@gmail.com>
2022-07-04 14:34:23 -0300

    calculadora v1.3

--- Calculadora/Simples/script.js
+++ Calculadora/Simples/script.js
@@ -1,36 +1,44 @@
 $(document).ready(function () {
     let num = '';
 
-    // Deleta itens
+    // Deleta itens.
     $("#del").click(() => {
         num = '';
-        $("#painel").text('');
+        $("#painel").text('0');
         tamanhoCampo(num);
     });
 
+    // Apaga um único item.
     $("#ac").click(() => {
         num = num.substring(0, num.length - 1); //extrai a string.
-        $("#painel").text(num);
+        if (num == '0' && num.length == 1) num = ''; //Ao apagar o ponto inserido com o 0.
+        if (num.length == 0) $("#painel").text('0');
+        else $("#painel").text(num);
         tamanhoCampo(num);
     });
-
+   
     // Adiciona números ao 'visor'.
-
-    //Se o 0 for o 1º nº.
-    $("#num-0").click(function () {
+    $("#num-0, #num-1, #num-2, #num-3, #num-4, #num-5, #num-6, #num-7, #num-8, #num-9, #dot").click(function () {
         num += $(this).text();
-        $("#painel").text(num);
-        if (num.length == 1) num = '';
+        console.log(num)
+        if (num != '.') $("#painel").text(num); //Evita que o ponto seja um caracter único ou primeiro número.
+
+        switch (num) {
+            case '.':
+                num = '0.'
+                $("#painel").text(num);
+                break;
+        
+            case '0': 
+                num = '';
+
+            default:
+                break;
+        }
         tamanhoCampo(num);
     });
-    
-    $("#num-1, #num-2, #num-3, #num-4, #num-5, #num-6, #num-7, #num-8, #num-9, #dot").click(function () {
-        num += $(this).text();
-        $("#painel").text(num);
-        tamanhoCampo(num);
-    });
 
-    // Operações
+    // Operações.
     $("#add, #sub, #mult, #div").click(function () {
         switch ($(this).text()) {
             case '+':
@@ -57,9 +65,9 @@ $(document).ready(function () {
                 break;
         }
     });
-    
+
     //Máximo de caracteres permitido.
     function tamanhoCampo(nm) {
-        if (nm.length > 16) num = nm.substring(0, 16);
+        if (nm.length >= 16) num = nm.substring(0, 15);
     }
 });
